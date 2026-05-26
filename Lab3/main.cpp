3c540000958815ab6f737878e00e5d916bcf896e
To Ngoc Tien <24280109@student.hcmus.edu.vn>
2026-05-26 11:40:33 +0700

    feat: added Section and Course classes with baseline

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <string>
+#include <list>
+#include <map>
 
 using namespace std;
+
+class Person;
+class Student;
+class Professor;
+class Course;
+class Section;
 
 // -------------------Class Person -------------------
 class Person {
@@ -137,6 +145,154 @@ void Student::display() {
     cout << "Name: " << this->getName() << endl;
     cout << "Major: " << this->getMajor() << endl;
     cout << "Degree: " << this->getDegree() << endl;
+}
+
+// -------------------Class Section -------------------
+class Section {
+    string sectionNo;
+    string dayOfWeek;
+    string timeOfDay;
+    string room;
+    int seatingCapacity;
+    Course *course;
+
+    void setCourse(Course *course);
+public:
+    Section(
+        string sectionNo, string dayOfWeek, string timeOfDay, 
+        string room, int seatingCapacity, Course *course
+    );
+
+    string getSectionNo();
+    string getDayOfWeek();
+    string getTimeOfDay();
+    string getRoom();
+    int getSeatingCapacity();
+
+    void setSectionNo(string sectionNo);
+    void setDayOfWeek(string dayOfWeek);
+    void setTimeOfDay(string timeOfDay);
+    void setRoom(string room);
+    void setSeatingCapacity(int seatingCapacity);
+};
+
+Section::Section(
+    string sectionNo, string dayOfWeek, string timeOfDay, 
+    string room, int seatingCapacity, Course *course
+) {
+    this->setSectionNo(sectionNo);
+    this->setDayOfWeek(dayOfWeek);
+    this->setTimeOfDay(timeOfDay);
+    this->setRoom(room);
+    this->setSeatingCapacity(seatingCapacity);
+    this->setCourse(course);
+}   
+
+string Section::getSectionNo() { return this->sectionNo; }
+string Section::getDayOfWeek() { return this->dayOfWeek; }
+string Section::getTimeOfDay() { return this->timeOfDay; }
+string Section::getRoom() { return this->room; }
+int Section::getSeatingCapacity() { return this->seatingCapacity; }
+
+void Section::setCourse(Course *course) {
+    this->course = course;
+}
+
+void Section::setSectionNo(string sectionNo) {
+    this->sectionNo = sectionNo; 
+}
+void Section::setDayOfWeek(string dayOfWeek) {
+    this->dayOfWeek = dayOfWeek; 
+}
+void Section::setTimeOfDay(string timeOfDay) {
+    this->timeOfDay = timeOfDay; 
+}
+void Section::setRoom(string room) {
+    this->room = room; 
+}
+void Section::setSeatingCapacity(int seatingCapacity) {
+    this->seatingCapacity = seatingCapacity; 
+}
+
+
+
+// -------------------Class Course -------------------
+class Course {
+    string courseNo;
+    string courseName;
+    int credits;
+    list<Course *> prerequistes;
+    map<string, Section *> sections;
+
+public:
+    Course(string courseNo, string courseName, int credits);
+
+    string getCourseNo();
+    string getCourseName();
+    int getCredits();
+
+    void setCourseName(string courseName);
+    void setCredits(int credits);
+    void setCourseNo(string courseNo);
+
+    void addPrerequisite(Course *course);
+    Section* scheduleSection(
+        string sectionNo, string dayOfWeek, string timeOfDay, 
+        string room, int seatingCapacity
+    );
+
+    ~Course();
+};
+
+Course::Course(string courseNo, string courseName, int credits) {
+    this->setCourseNo(courseNo);
+    this->setCourseName(courseName);
+    this->setCredits(credits);
+}
+
+string Course::getCourseNo() {
+    return this->courseNo;
+}
+
+string Course::getCourseName() {
+    return this->courseName;
+}
+
+int Course::getCredits() {
+    return this->credits;
+}
+
+void Course::setCourseNo(string courseNo) {
+    this->courseNo = courseNo;
+}
+
+void Course::setCourseName(string courseName) {
+    this->courseName = courseName;
+}
+
+void Course::setCredits(int credits) {
+    this->credits = credits;
+}
+
+void Course::addPrerequisite(Course *course) {
+    this->prerequistes.push_back(course);
+}
+
+Section* Course::scheduleSection(
+    string sectionNo, string dayOfWeek, string timeOfDay, 
+    string room, int seatingCapacity
+) {
+    Course *course = this;
+    Section *section = new Section(sectionNo, dayOfWeek, timeOfDay, room, seatingCapacity, course);
+
+    this->sections[sectionNo] = section;
+}
+
+Course::~Course() {
+    for(auto& pair : sections) {
+        delete(pair.second);
+    }
+    sections.clear();
 }
 
 int main() {
